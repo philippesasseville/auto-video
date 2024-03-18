179c6509410e9e7f9c9d7709e304c12114b27476
Philippe Sasseville <philippe.sasseville@yaksa.ca>
2024-03-17 23:38:35 -0400

    add folders and remove failed video logic

--- automatic_tiktok_uploader.py
+++ automatic_tiktok_uploader.py
@@ -5,6 +5,3 @@ from tiktok_uploader.auth import AuthBackend
 def upload_videos(videos):
     auth = AuthBackend(cookies='cookies/cookies.txt')
     failed_videos = upload_videos(videos=videos, auth=auth)
-
-    for video in failed_videos: # each input video object which failed
-        print(f'{video['video']} with description "{video['description']}" failed')
